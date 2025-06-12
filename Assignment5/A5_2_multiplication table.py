8566835674c6bee6320cca8c1097c3aa608eb2b2
amene ahani <ahanir@ymail.com>
2025-06-12 12:05:02 +0330

    Assignment4

--- Assignment5/A5_2_multiplication table.py
+++
@@ -1,9 +0,0 @@
-#multiplication table n*m
-
-def multy_table(n: int, m: int):
-    for row in range(1,n+1):
-        for column in range(1,m+1):
-            print(row*column, end="  ")
-        print("")
-
-multy_table(int(input("Please enter n: ")), int(input("please enter m: ")))
